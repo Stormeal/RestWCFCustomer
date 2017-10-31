4a0152bed59b849034776a46acdd1f55a18b22f8
Alex Storm <alex6614@edu.easj.dk>
2017-10-31 12:57:32 +0100

    Initial push

--- RestWCFCustomer/ICustomerService.cs
+++ RestWCFCustomer/ICustomerService.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace RestWCFCustomer
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICustomerService" in both code and config file together.
+    // NOTE: You can use the "Rename", command on the "Refactor" menu to change the interface name "ICustomerService" in both code and config file together.
     [ServiceContract]
     public interface ICustomerService
     {
